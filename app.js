45ff1848b9a370588e4d8fafcfa825177269b5d8
Rome Bell <romebell@Romes-MacBook-Pro.local>
2020-07-15 17:03:47 -0400

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = 'Rome';
+console.log(name);
+
+var myLocation = 'Los Angeles';
+console.log(myLocation);
+
+var favoriteNumber = 5;
+console.log(favoriteNumber);
+
+var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets'];
+console.log(teams);
+
